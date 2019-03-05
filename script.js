50a99ba7ce0ae79647883c58d7394bb5c1fcd073
Eder Marques <31988308+L1224rd@users.noreply.github.com>
2019-03-04 23:44:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-
-const rawData = STRING_FROM_SHEETS.split('\n').map(line => line.split('\t'));
-const data = [];
-rawData.forEach((line, i) => {
-  if (!i) return;
-  data.push({});
-  line.forEach((column, j) => {
-    data[i - 1][rawData[0][j]] = rawData[i][j];
+const sheetToJson = (sheetString) => {
+  const rawData = sheetString.split('\n').map(line => line.split('\t'));
+  const data = [];
+  
+  rawData.forEach((line, i) => {
+    if (!i) return;
+    data.push({});
+    line.forEach((column, j) => {
+      data[i - 1][rawData[0][j]] = rawData[i][j];
+    });
   });
-});
-
-console.log(JSON.stringfy(data));
+  
+  return data;
+};
